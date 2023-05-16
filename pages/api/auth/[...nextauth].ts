857f7ddafc4d74eca0aab1bf7278f581e9df9f4a
david emioma <davidemiomauche@gmail.com>
2023-05-16 22:36:46 +0100

    Setup next auth with prisma

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,57 @@
+import bcrypt from "bcrypt";
+import prisma from "../../../lib/prismaDb";
+import NextAuth, { AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials!");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid credentials!");
+        }
+
+        const pwdMatched = await bcrypt.compare(
+          credentials?.password,
+          user.hashedPassword
+        );
+
+        if (!pwdMatched) {
+          throw new Error("Invalid password!");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
